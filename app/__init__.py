0c2f57137f8e57867c1c1c7f12e7ea4d756fc2f1
Eb-Zeero <macebelene@gmail.com>
2023-09-12 11:32:59 +0200

    Adding General Specta Front part

---
+++ app/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+# Create a blueprint for the tabs
+from flask import Blueprint
+
+tabs = Blueprint('/', __name__)
+
+
+@tabs.route('/')
+def general():
+    return render_template('general.html')
+
+
+@tabs.route('/configure')
+def configure():
+    return render_template('configure.html')
+
+
+@tabs.route('/exposure')
+def exposure():
+    return render_template('exposure.html')
+
+
+app.register_blueprint(tabs)
+
+if __name__ == '__main__':
+    app.run(debug=True)
