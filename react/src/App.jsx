3ab81309d7caf77051b34ae860a29b737fb76869
Eb-Zeero <macebelene@gmail.com>
2023-10-12 12:24:16 +0200

    Plotting a fake plot

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,6 +1,14 @@
 import {Tabs} from "./components/Tabs.jsx";
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement } from "chart.js";
 
 function App() {
+    // Register the plugin to all charts:
+    ChartJS.register(
+        CategoryScale,
+        LinearScale,
+        PointElement,
+        LineElement
+    );
   return (
       <div className="container">
         <Tabs />
