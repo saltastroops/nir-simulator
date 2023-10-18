b1e8232fa94fcf67ab8179d1add59802fde7e512
Christian Hettlage <hettlage@saao.ac.za>
2023-10-18 17:38:23 +0200

    Change option types names

--- react/src/components/plots/PlotOptions.ts
+++ react/src/components/plots/PlotOptions.ts
@@ -1,46 +1,51 @@
-interface  Options {
-    type: string;
-    title: {
-        display: boolean;
-        text: string;
-    },
-    ticks: {
+interface ScaleOptions {
+  type: string;
+  title: {
+    display: boolean;
+    text: string;
+  };
+  ticks:
+    | {
         min: number;
         max: number;
         stepSize: number;
-    } | undefined
+      }
+    | undefined;
 }
-interface ScaleOptions{
-    x:  Options;
-    y: Options;
+interface ScalesOptions {
+  x: ScaleOptions;
+  y: ScaleOptions;
 }
 
-interface LineOptions{
-    scales: ScaleOptions;
+interface LineOptions {
+  scales: ScalesOptions;
 }
-export function defaultLinePlotOptions (xLabel: string, yLabel: string): LineOptions {
-    return {
-        scales: {
-            x: {
-                type: "linear",
-                title: {
-                    display: true,
-                    text: xLabel
-                },
-                ticks: {
-                    min: 8000,
-                    max: 18000,
-                    stepSize: 1000
-                }
-            },
-            y: {
-                type: "linear",
-                title: {
-                    display: true,
-                    text: yLabel
-                },
-                ticks: undefined
-            },
+export function defaultLinePlotOptions(
+  xLabel: string,
+  yLabel: string,
+): LineOptions {
+  return {
+    scales: {
+      x: {
+        type: "linear",
+        title: {
+          display: true,
+          text: xLabel,
         },
-    }
+        ticks: {
+          min: 8000,
+          max: 18000,
+          stepSize: 1000,
+        },
+      },
+      y: {
+        type: "linear",
+        title: {
+          display: true,
+          text: yLabel,
+        },
+        ticks: undefined,
+      },
+    },
+  };
 }
