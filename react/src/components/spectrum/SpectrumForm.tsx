b13ec0a561c087186766376dd2acb47fbfb81aef
Christian Hettlage <hettlage@saao.ac.za>
2023-10-18 10:44:36 +0200

    Replace link with button

--- react/src/components/spectrum/SpectrumForm.tsx
+++ react/src/components/spectrum/SpectrumForm.tsx
@@ -9,11 +9,16 @@ export default function SpectrumForm({
   children,
 }: PropsWithChildren<Props>) {
   return (
-    <div className="border border-gray-200 p-2 mt-3">
+    <div className="relative border border-gray-200 p-2 mt-3">
+      <div
+        className="absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 border border-red-500 rounded-full bg-red-500 text-white w-6 h-6 flex justify-center items-center cursor-pointer"
+        onClick={remove}
+      >
+        <div>
+          <i className="fa-solid fa-xmark"></i>
+        </div>
+      </div>
       {children}
-      <span className="hover:cursor-pointer text-red-500" onClick={remove}>
-        Delete
-      </span>
     </div>
   );
 }
