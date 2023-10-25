b5759a0f11f5e7bd08f29776696ab18cf47b46d1
Eb-Zeero <macebelene@gmail.com>
2023-10-25 13:34:53 +0200

    Working need to fix calculations

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.urls import path, include
 
 from nirwals.views.throughput import throughput
+from nirwals.views.exposure import solve_for_snr
 
 
 def home(request):
@@ -13,5 +14,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home, name="home"),
     path("__reload__/", include("django_browser_reload.urls")),
-    path("throughput/", throughput, name='throughput')
+    path("throughput/", throughput, name='throughput'),
+    path("solve/", solve_for_snr, name='solve-snr')
 ]
--- react/src/components/exposure/Exposure.tsx
+++ react/src/components/exposure/Exposure.tsx
@@ -1,9 +1,56 @@
+import { LinePlot } from "../plots/LinePlot.tsx";
+import { useState } from "react";
+import { defaultLinePlotOptions } from "../plots/PlotOptions.ts";
+import { ChartContent } from "../instrument/InstrumentConfigurationPanel.tsx";
+import { environment } from "../../environments/environment.ts";
 
 export function Exposure() {
-    return (
-        <div>
-            <h1 className="title is-1">Make An Exposure</h1>
-            <p>The content of Make An Exposure goes here</p>
-        </div>
-    );
+  const [chartContent, setChartContent] = useState<ChartContent>({
+    chartData: {
+      x: [],
+      y: [],
+      lineColor: "rgb(75, 192, 192)",
+      options: defaultLinePlotOptions("Wavelength (\u212B)", "Throughput"),
+    },
+    requested: false,
+  });
+
+  const updatePlot = () => {
+    // const formData = new FormData();
+    fetch(environment.apiUrl + "/solve/", {
+      method: "GET",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Opps, Something went wrong.");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setChartContent((previousChartContent) => {
+          const updatedChartData = {
+            x: data.x,
+            y: data.y,
+            lineColor: previousChartContent.chartData.lineColor,
+            options: previousChartContent.chartData.options,
+          };
+          return {
+            chartData: updatedChartData,
+            requested: true,
+          };
+        });
+      })
+      .catch((err) => {
+        console.error("Error fetching plot data:", err);
+      });
+  };
+
+  return (
+    <div>
+      <h1 className="title is-1">Make An Exposure</h1>
+      <p>The content of Make An Exposure goes here</p>
+      <LinePlot chartContent={chartContent} isOutdated={false} />
+      <button onClick={() => updatePlot()}>Update</button>
+    </div>
+  );
 }
