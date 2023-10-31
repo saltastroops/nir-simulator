1486f1ffe16c124a3c11ee7d0a0890372faff1b1
mofokeng-chaka <mofokeng.chk@gmail.com>
2023-10-31 09:26:51 +0200

    Review fix: properly calculate photon counts

--- backend/nirwals/configure/data/spectrum.py
+++ backend/nirwals/configure/data/spectrum.py
@@ -4,6 +4,10 @@ from functools import lru_cache
 import numpy as np
 from os import getenv
 from scipy.constants import Planck, speed_of_light, Boltzmann
+from specutils import Spectrum1D
+from specutils.manipulation import FluxConservingResampler
+from astropy import units as u
+from nirwals.utils import get_redshifted_spectrum
 
 FILES_BASE_DIR = pathlib.Path(getenv("FILES_BASE_DIR"))
 
@@ -20,20 +24,17 @@ conv = {
 
 
 def get_stellar_flux_values(wavelength: float, temperature: float, mag: float):
-    flux = np.zeros(40001)
     star_black_body_radiation = ((2 * np.pi * h * c**2) / wavelength**5) * 1/(np.exp((h*c)/(wavelength*kb*temperature)) - 1)
 
     star_magnitude_flux = 3.02 * 10 ** (-10) * 10 ** (-0.4 * mag)
-    normalizer = (star_black_body_radiation[18000]) / star_magnitude_flux
+    normalizer = max(star_black_body_radiation) / star_magnitude_flux
 
     normalized_star_radiation = star_black_body_radiation/normalizer
     photon_count = (normalized_star_radiation / ((h * c) / wavelength))
-    flux = flux + photon_count
-    return flux
+    return photon_count
 
 
 def get_galaxy_flux_values(wavelength: [], galaxy_type: str, age: str, has_emission_line: bool, magnitude: float, redshift: float):
-    flux = np.zeros(40001)
     galaxy_types = ["E", "Sb", "Sa", "Sc", "Sd", "S0"]
     age_types = ["Young", "Old"]
 
@@ -42,30 +43,36 @@ def get_galaxy_flux_values(wavelength: [], galaxy_type: str, age: str, has_emiss
         file_path = FILES_BASE_DIR / "data_sheets" / "adjusted_program_datasheets" / filename
         galaxy_wavelength, galaxy_flux = read_csv_file(file_path)
 
-        galaxy_wavelength = galaxy_wavelength * (1 + redshift)
-        galaxy_flux = galaxy_flux / (1 + redshift)
+        galaxy_wavelength, galaxy_flux = get_redshifted_spectrum(galaxy_wavelength, galaxy_flux, redshift)
 
-        starting_index = np.nonzero(np.isin(wavelength, galaxy_wavelength))[0][0]
+        input_wavelength = galaxy_wavelength * u.AA
+        input_flux = galaxy_flux * u.Unit('erg cm-2 s-1 AA-1')
+        input_spectrum = Spectrum1D(spectral_axis=input_wavelength, flux=input_flux)
 
-        selected_galaxy_radiation = galaxy_flux[starting_index:starting_index+40001]
+        fluxcon = FluxConservingResampler()
+
+        new_disp_grid = wavelength * u.AA
+
+        new_resampled_spectrum = fluxcon(input_spectrum, new_disp_grid)
+
         galaxy_magnitude_flux = 3.02 * 10 ** (-10) * 10 ** (-0.4 * magnitude)
-        normalizer = selected_galaxy_radiation[18001] / galaxy_magnitude_flux
 
-        normalized_star_radiation = selected_galaxy_radiation/normalizer
+        normalizer = max(new_resampled_spectrum.flux) / galaxy_magnitude_flux
+
+        normalized_star_radiation = new_resampled_spectrum.flux/normalizer
         photon_count = (normalized_star_radiation / ((h * c) / wavelength))
-        flux = flux + photon_count
-        return flux
+
+        return photon_count
 
 
-def get_emission_line_values(wavelength: float, line_flux: float, lamda: float, line_fwhm: float, redshift: float):
-    flux = np.zeros(40001)
-    central_wavelength = lamda * (1 + redshift)
+def get_emission_line_values(wavelength: [], line_flux: float, lamda: float, line_fwhm: float, redshift: float):
+    redshifted_central_wavelength, redshifted_line_flux = get_redshifted_spectrum(lamda, line_flux, redshift)
     line_signal = line_fwhm / 2.35
 
-    line_profile = line_flux * 1 / (line_signal * np.sqrt(2 * np.pi)) * np.exp(-((wavelength - central_wavelength) ** 2 / (2 * line_signal**2)))
+    line_profile = redshifted_line_flux * 1 / (line_signal * np.sqrt(2 * np.pi)) * np.exp(-((wavelength - redshifted_central_wavelength) ** 2 / (2 * line_signal**2)))
 
-    flux = flux + line_profile / ((h * c) / wavelength)
-    return flux
+    photon_count = line_profile / ((h * c) / wavelength)
+    return photon_count
 
 
 #  TODO Caching should be done Correctly
@@ -113,21 +120,21 @@ def get_sources_spectrum(form_data):
     ])
 
     data['wavelength'] = np.array([9000 + i / 5 for i in range(num_points)])
-    data['sources_flux'] = np.ones(num_points)
+    data['sources_flux'] = np.zeros(num_points)
 
     sources = parameters["source"]["spectrum"]
 
     for source in sources:
         if source["spectrumType"] == "Blackbody":
             star_flux = get_stellar_flux_values(data['wavelength'], float(source["temperature"]), float(source["magnitude"]))
-            data['sources_flux'] = data['sources_flux'] * star_flux
-        if source["spectrumType"] == "Galaxy":
+            data['sources_flux'] = data['sources_flux'] + star_flux
+        elif source["spectrumType"] == "Galaxy":
             has_emission_line = False
             galaxy_flux = get_galaxy_flux_values(data['wavelength'], source['type'], source["age"], has_emission_line, float(source["magnitude"]), float(source["redshift"]))
-            data['sources_flux'] = data['sources_flux'] * galaxy_flux
-        if source["spectrumType"] == "Emission Line":
+            data['sources_flux'] = data['sources_flux'] + galaxy_flux
+        elif source["spectrumType"] == "Emission Line":
             emission_line_flux = get_emission_line_values(data['wavelength'], float(source["flux"]), float(source["centralWavelength"]), float(source["fwhm"]), float(source["redshift"]))
-            data['sources_flux'] = data['sources_flux'] * emission_line_flux
+            data['sources_flux'] = data['sources_flux'] + emission_line_flux
 
     if parameters["spectrumPlotOptions"]["includeAtmosphericExtinction"]:
         filename = FILES_BASE_DIR / "data_sheets" / "adjusted_program_datasheets" / "nirskytransmission.csv"
@@ -137,11 +144,11 @@ def get_sources_spectrum(form_data):
         if parameters["spectrumPlotOptions"]["calculateFluxInSeeingDisk"]:
             point_spread = 1 / (np.pi * float(parameters["earth"]["seeing"]) ** 2 / 4)
             data['sources_flux'] = data['sources_flux'] * point_spread
-    if parameters["spectrumPlotOptions"]["multiplyWithMirrorAreaAndEfficiency"] == "true":
+    if parameters["spectrumPlotOptions"]["multiplyWithMirrorAreaAndEfficiency"]:
         filename = FILES_BASE_DIR / "data_sheets" / "adjusted_program_datasheets" / "combinedtelescope.csv"
         wavelength, telescope_flux = read_csv_file(filename)
 
         data['sources_flux'] = data['sources_flux'] * telescope_flux
-        data['sources_flux'] = data['sources_flux'] * int(parameters["mirror_area"])
+        data['sources_flux'] = data['sources_flux'] * int(parameters["earth"]["mirrorArea"])
 
     return data['wavelength'], data['sources_flux']
