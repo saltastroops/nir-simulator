aee6cc060aceb6d646dfd6f5d1ed927a4a756834
mofokeng-chaka <mofokeng.chk@gmail.com>
2023-11-23 13:11:45 +0200

    Add clear spectrum button

--- react/src/components/spectrum/SpectrumGenerationTab.tsx
+++ react/src/components/spectrum/SpectrumGenerationTab.tsx
@@ -103,6 +103,39 @@ export function SpectrumGenerationTab({ setup, updateSetup }: Props) {
     }
   };
 
+  const clearPlots = async () => {
+    try {
+      setChartContent((previousChartContent) => {
+        const updatedChartData = {
+          x: [],
+          y: [],
+          lineColor: previousChartContent.chartData.lineColor,
+          options: previousChartContent.chartData.options,
+        };
+        return {
+          chartData: updatedChartData,
+          requested: false,
+        };
+      });
+
+      setSecondChartContent((previousChartContent) => {
+        const updatedChartData = {
+          x: [],
+          y: [],
+          lineColor: previousChartContent.chartData.lineColor,
+          options: previousChartContent.chartData.options,
+        };
+        return {
+          chartData: updatedChartData,
+          requested: false,
+        };
+      });
+    } catch (error) {
+      setError("Data request failed.");
+      console.error("Error fetching plot data:", error);
+    }
+  };
+
   return (
     <div className="columns">
       <div className="column">
@@ -150,6 +183,13 @@ export function SpectrumGenerationTab({ setup, updateSetup }: Props) {
             onClick={updatePlots}
           >
             Show Spectrum
+          </button>
+
+          <button
+              className={button("mt-6 text-white bg-red-600")}
+              onClick={clearPlots}
+          >
+            Clear Spectrum
           </button>
         </div>
       </div>
