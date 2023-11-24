684d46ba6e5d05998afd59890e52ac5745301fc3
mofokeng-chaka <mofokeng.chk@gmail.com>
2023-11-24 13:15:26 +0200

    Review fix: remove redundant button

--- react/src/components/spectrum/SpectrumGenerationTab.tsx
+++ react/src/components/spectrum/SpectrumGenerationTab.tsx
@@ -106,39 +106,6 @@ export function SpectrumGenerationTab({ setup, updateSetup }: Props) {
     }
   };
 
-  const clearPlots = async () => {
-    try {
-      setChartContent((previousChartContent) => {
-        const updatedChartData = {
-          x: [],
-          y: [],
-          lineColor: previousChartContent.chartData.lineColor,
-          options: previousChartContent.chartData.options,
-        };
-        return {
-          chartData: updatedChartData,
-          requested: false,
-        };
-      });
-
-      setSecondChartContent((previousChartContent) => {
-        const updatedChartData = {
-          x: [],
-          y: [],
-          lineColor: previousChartContent.chartData.lineColor,
-          options: previousChartContent.chartData.options,
-        };
-        return {
-          chartData: updatedChartData,
-          requested: false,
-        };
-      });
-    } catch (error) {
-      setError("Data request failed.");
-      console.error("Error fetching plot data:", error);
-    }
-  };
-
   return (
     <div className="columns">
       <div className="column">
@@ -186,13 +153,6 @@ export function SpectrumGenerationTab({ setup, updateSetup }: Props) {
             onClick={updatePlots}
           >
             Show Spectrum
-          </button>
-
-          <button
-              className={button("mt-6 text-white bg-red-600")}
-              onClick={clearPlots}
-          >
-            Clear Spectrum
           </button>
         </div>
       </div>
