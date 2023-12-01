1878992c15853b1233c0e0efb3381e8197f3a04d
mofokeng-chaka <mofokeng.chk@gmail.com>
2023-12-01 13:56:30 +0200

    Use synphot classes and methods

--- backend/nirwals/configure/data/throughput.py
+++ backend/nirwals/configure/data/throughput.py
@@ -1,22 +1,12 @@
-import csv
 import pathlib
-from functools import lru_cache
 
 import numpy as np
 from os import getenv
-from scipy.special import erf
+from synphot import SourceSpectrum, units
+from astropy.modeling.functional_models import Const1D
+from nirwals.utils import read_csv_file
 
 FILES_BASE_DIR = pathlib.Path(getenv("FILES_BASE_DIR"))
-
-
-#  TODO Caching should be done Correctly
-@lru_cache
-def read_csv_file(filename):
-    with open(filename, 'r') as file:
-        reader = csv.reader(file, delimiter=",", quotechar="|")
-        data = list(reader)
-        wavelength, modifier = zip(*[(float(x), float(y)) for x, y in data])
-    return np.array(wavelength), np.array(modifier)
 
 
 def get_slit_modifier(constant):
@@ -52,25 +42,26 @@ def get_modifiers(configuration):
     data = np.empty(num_points, dtype=[
         ('wavelength', float),
         ('throughput', float),
-        ('modifier', float),
-        ('modified_throughput', float)
     ])
-    data['wavelength'] = np.arange(1, num_points + 1)
-    data['throughput'] = np.ones(num_points)
+    data['wavelength'] = np.linspace(9000, 17000, num_points)
+
+    throughput_spectrum = SourceSpectrum(Const1D, amplitude=1 * units.THROUGHPUT)
 
     if configuration["mode"] == "Spectroscopy":
         slit_width = float(configuration["slit_width"])
         target_zd = float(configuration["target_zd"])
-        slit_losses = erf(
-            (slit_width * np.sqrt(np.log(2))) / np.sqrt(
-                0.6**2 + ((1 / np.cos(target_zd * np.pi / 180))**(3 / 5) * 1)**2))
-        data['wavelength'], data['throughput'] = get_slit_modifier(slit_losses)
+        seeing = float(configuration["seeing"])
+        sigma_squared = (1 / (8*np.log(2))) * (seeing**2 * (1 / np.cos(target_zd * np.pi / 180))**(6/5) + 0.6**2)
+        slit_losses = 1 - np.exp(-(slit_width/2)**2/sigma_squared)
+
+        throughput_spectrum = SourceSpectrum(Const1D, amplitude=slit_losses * units.THROUGHPUT)
 
     for filename in get_affected_filenames(configuration):
         wavelength, modifier = read_csv_file(filename)
+        modifier_spectrum = SourceSpectrum(Const1D, amplitude=modifier * units.THROUGHPUT)
+
         data['wavelength'] = wavelength
-        data['modifier'] = modifier
-        data['throughput'] = data['modifier'] * data['throughput']
+        data['throughput'] = throughput_spectrum(wavelength) * modifier_spectrum(wavelength)
 
     return data['wavelength'], data['throughput']
 
