a8b209473c39739aa82bfa70e1633feceb70c656
hettlage <hettlage@saao.ac.za>
2023-12-19 21:34:41 +0200

    Docker deploy (#34)
    Add Docker files
    
    Add a deployment script
    
    ---------
    
    Co-authored-by: Eb-Zeero <macebelene@gmail.com>

--- backend/nirwals/configure/data/throughput.py
+++ backend/nirwals/configure/data/throughput.py
@@ -6,13 +6,11 @@ import numpy as np
 from os import getenv
 from scipy.special import erf
 
-FILES_BASE_DIR = pathlib.Path(getenv("FILES_BASE_DIR"))
-
 
 #  TODO Caching should be done Correctly
 @lru_cache
 def read_csv_file(filename):
-    with open(filename, 'r') as file:
+    with open(filename, "r") as file:
         reader = csv.reader(file, delimiter=",", quotechar="|")
         data = list(reader)
         wavelength, modifier = zip(*[(float(x), float(y)) for x, y in data])
@@ -28,51 +26,87 @@ def get_slit_modifier(constant):
 
 def get_affected_filenames(form_data):
     filenames = [
-        FILES_BASE_DIR / "data_sheets" / "adjusted_program_datasheets" / "detectorqe.csv",
-        FILES_BASE_DIR / "data_sheets" / "adjusted_program_datasheets" / "combinedtelescope.csv"
+        pathlib.Path("data")
+        / "data_sheets"
+        / "adjusted_program_datasheets"
+        / "detectorqe.csv",
+        pathlib.Path("data")
+        / "data_sheets"
+        / "adjusted_program_datasheets"
+        / "combinedtelescope.csv",
     ]
     if form_data["mode"] == "Imaging":
         if form_data["filter"] == "clear-filter":
-            filenames.append(FILES_BASE_DIR  / "data_sheets" /"adjusted_program_datasheets"/"clearfiltertransmission.csv")
+            filenames.append(
+                pathlib.Path("data")
+                / "data_sheets"
+                / "adjusted_program_datasheets"
+                / "clearfiltertransmission.csv"
+            )
         elif form_data["filter"] == "lwbf":
-            filenames.append(FILES_BASE_DIR / "data_sheets" / "adjusted_program_datasheets" / "lwbftransmission.csv")
+            filenames.append(
+                pathlib.Path("data")
+                / "data_sheets"
+                / "adjusted_program_datasheets"
+                / "lwbftransmission.csv"
+            )
     elif form_data["mode"] == "Spectroscopy":
         if form_data["filter"] == "clear-filter":
-            filenames.append(FILES_BASE_DIR / "data_sheets" / "adjusted_program_datasheets" / "clearfiltertransmission.csv")
+            filenames.append(
+                pathlib.Path("data")
+                / "data_sheets"
+                / "adjusted_program_datasheets"
+                / "clearfiltertransmission.csv"
+            )
         elif form_data["filter"] == "lwbf":
-            filenames.append(FILES_BASE_DIR / "data_sheets" / "adjusted_program_datasheets" / "lwbftransmission.csv")
+            filenames.append(
+                pathlib.Path("data")
+                / "data_sheets"
+                / "adjusted_program_datasheets"
+                / "lwbftransmission.csv"
+            )
         if form_data["grating"] == "950":
             filenames.append(
-                FILES_BASE_DIR / "data_sheets" / "adjusted_program_datasheets" / f"tempVPH{form_data['grating_angle']}.csv")
+                pathlib.Path("data")
+                / "data_sheets"
+                / "adjusted_program_datasheets"
+                / f"tempVPH{form_data['grating_angle']}.csv"
+            )
     return list(set(filenames))
 
 
 def get_modifiers(configuration):
     num_points = 40001
-    data = np.empty(num_points, dtype=[
-        ('wavelength', float),
-        ('throughput', float),
-        ('modifier', float),
-        ('modified_throughput', float)
-    ])
-    data['wavelength'] = np.arange(1, num_points + 1)
-    data['throughput'] = np.ones(num_points)
+    data = np.empty(
+        num_points,
+        dtype=[
+            ("wavelength", float),
+            ("throughput", float),
+            ("modifier", float),
+            ("modified_throughput", float),
+        ],
+    )
+    data["wavelength"] = np.arange(1, num_points + 1)
+    data["throughput"] = np.ones(num_points)
 
     if configuration["mode"] == "Spectroscopy":
         slit_width = float(configuration["slit_width"])
         target_zd = float(configuration["target_zd"])
         slit_losses = erf(
-            (slit_width * np.sqrt(np.log(2))) / np.sqrt(
-                0.6**2 + ((1 / np.cos(target_zd * np.pi / 180))**(3 / 5) * 1)**2))
-        data['wavelength'], data['throughput'] = get_slit_modifier(slit_losses)
+            (slit_width * np.sqrt(np.log(2)))
+            / np.sqrt(
+                0.6**2 + ((1 / np.cos(target_zd * np.pi / 180)) ** (3 / 5) * 1) ** 2
+            )
+        )
+        data["wavelength"], data["throughput"] = get_slit_modifier(slit_losses)
 
     for filename in get_affected_filenames(configuration):
         wavelength, modifier = read_csv_file(filename)
-        data['wavelength'] = wavelength
-        data['modifier'] = modifier
-        data['throughput'] = data['modifier'] * data['throughput']
+        data["wavelength"] = wavelength
+        data["modifier"] = modifier
+        data["throughput"] = data["modifier"] * data["throughput"]
 
-    return data['wavelength'], data['throughput']
+    return data["wavelength"], data["throughput"]
 
 
 def get_throughput_plot_data(configuration):
