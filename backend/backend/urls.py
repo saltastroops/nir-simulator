1735e80ecb291132b817f513e3f1c8ba667f2363
Christian Hettlage <hettlage@saao.ac.za>
2023-12-20 19:43:35 +0200

    Remove obsolete function

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
-from django.shortcuts import render
 from django.urls import path
 
 from nirwals.views import spectrum_view, throughput_view
-
-
-def home(request):
-    return render(request, "index.html", {})
 
 
 urlpatterns = [
