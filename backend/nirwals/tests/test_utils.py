5d7616b0ce4982ed52071014cf908507c67a5d5a
Christian Hettlage <hettlage@saao.ac.za>
2023-12-21 22:22:39 +0200

    Add a function for preparing the data for spectrum plots

--- backend/nirwals/tests/test_utils.py
+++ backend/nirwals/tests/test_utils.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pytest
+from _pytest.monkeypatch import MonkeyPatch
 from astropy import units as u
 from synphot import units
 
 from nirwals.physics.utils import read_from_file, shift
 from nirwals.tests.utils import get_default_datafile
+from nirwals.utils import prepare_spectrum_plot_values, MAX_NUM_PLOT_POINTS
 
 
 def test_read_from_file() -> None:
@@ -37,3 +39,37 @@ def test_shift() -> None:
     assert np.array_equal(shift(a, 5), np.array([0, 0, 0, 0, 0, 1, 2, 3, 4, 5]))
     assert np.array_equal(shift(a, -1), np.array([2, 3, 4, 5, 6, 7, 8, 9, 10, 0]))
     assert np.array_equal(shift(a, -3), np.array([4, 5, 6, 7, 8, 9, 10, 0, 0, 0]))
+
+
+def test_prepare_spectrum_plot_values_no_resampling(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr("nirwals.utils.get_minimum_wavelength", lambda: 10000 * u.AA)
+    monkeypatch.setattr("nirwals.utils.get_maximum_wavelength", lambda: 14000 * u.AA)
+
+    wavelengths = (
+        np.array([9000, 9500, 9999.9, 10000.1, 12000, 13999.9, 14000.1, 16000]) * u.AA
+    )
+    counts = np.array([56, 42, 99, 16, 9, 77, 14, 84]) * u.photon
+    xs, ys = prepare_spectrum_plot_values(wavelengths, counts, u.photon)
+
+    assert xs == [10000.1, 12000, 13999.9]
+    assert ys == [16, 9, 77]
+
+
+def test_prepare_spectrum_plot_values_with_resampling(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr("nirwals.utils.get_minimum_wavelength", lambda: 10000 * u.AA)
+    monkeypatch.setattr("nirwals.utils.get_maximum_wavelength", lambda: 14000 * u.AA)
+
+    max_num_points = MAX_NUM_PLOT_POINTS
+    wavelengths = np.linspace(9950, 14050, 6 * max_num_points) * u.AA
+    counts = 2 * np.linspace(9950, 14050, 6 * max_num_points) * u.photon
+    xs, ys = prepare_spectrum_plot_values(wavelengths, counts, u.photon)
+
+    expected_ys = [2 * x for x in xs]
+
+    assert len(xs) == max_num_points
+    assert len(ys) == max_num_points
+    assert 9999 < xs[0] < 10001
+    assert 13999 < xs[-1] < 14001
+    # Check that the points have the right distance from each other.
+    assert pytest.approx(xs[1] - xs[0]) == (xs[-1] - xs[0]) / (max_num_points - 1)
+    assert np.allclose(ys, expected_ys)
--- backend/nirwals/utils.py
+++ backend/nirwals/utils.py
@@ -3,45 +3,71 @@ from pathlib import Path
 from typing import Any
 
 import numpy as np
-from astropy.units import Quantity
-from specutils.manipulation import FluxConservingResampler
+from specutils.manipulation import LinearInterpolatedResampler
 from astropy import units as u
 from specutils import Spectrum1D
 
-
-NUMBER_OF_POINTS = 40001
-conv: Any = {
-    0: lambda x: float(x),  # conversion fn for column 0
-    1: lambda x: float(x),  # conversion fn for column 1
-}
+from constants import get_minimum_wavelength, get_maximum_wavelength
 
 
-def get_redshifted_spectrum(
-    wavelength: Any, flux: Any, redshift: float
-) -> tuple[Quantity, Quantity]:
-    wavelength = wavelength * (1 + redshift)
-    flux = flux / (1 + redshift)
-
-    return wavelength, flux
+MAX_NUM_PLOT_POINTS = 401
 
 
-#  TODO Caching should be done Correctly
-@lru_cache
-def read_csv_file(filename: Path) -> tuple[Any, Any]:
-    spectra_data = np.loadtxt(filename, delimiter=",", quotechar="|", converters=conv)
-    wavelength = spectra_data[:, 0]
-    flux = spectra_data[:, 1]
-    return wavelength, flux
+def prepare_spectrum_plot_values(
+    wavelengths: u.AA, y: u.Quantity, y_units: u.Unit
+) -> tuple[list[float], list[float]]:
+    """
+    Prepare values fior plotting in a plot of some quantity over wavelength.
 
+    The function takes three arguments: An array of wavelengths, an array of dependent
+    ("y") values, and the units in which the dependent values should be plotted.
 
-def resample_spectrum(
-    input_spectrum: Spectrum1D,
-    minimum_wavelength: float = 9000,
-    maximum_wavelength: float = 17000,
-    number_of_points: int = NUMBER_OF_POINTS,
-) -> Spectrum1D:
-    wavelength_range = (
-        np.linspace(minimum_wavelength, maximum_wavelength, number_of_points) * u.AA
+    All wavelengths outside the range from the minimum to the maximum supported
+    wavelength are ignored. If the remaining array is still longer than the maximum
+    number m of points to include in a plot, the wavelengths are resampled to m
+    equidistant wavelengths from the minimum to the maximum supported wavelength.
+    Linear interpolation is used to calculate the corresponding y values.
+
+    The resulting wavelengths and y values are returned as Python lists. They are
+    given in Angstrom (for the wavelengths) and the units specified by the y_units
+    argument (for the y values).
+
+    Parameters
+    ----------
+    wavelengths: Quantity
+        The wavelengths.
+    y: Quantity
+        The dependent values
+    y_units: astropy.unit.Unit
+        The units in which the dependent values should be plotted.
+
+    Returns
+    -------
+    tuple of lists of floats
+        The wavelengths (in Angstrom) and dependent values (in the units specified by
+        y_units) to plot.
+    """
+    # Convert the data to floats.
+    wavelength_values = wavelengths.to(u.AA).value
+    y_values = y.to(y_units).value
+
+    # Only keep values within the desired wavelength range. We include a "grace range"
+    # of 1 Angstrom at each boundary.
+    min_wavelength = get_minimum_wavelength().to(u.AA).value - 1
+    max_wavelength = get_maximum_wavelength().to(u.AA).value + 1
+    within_range = (wavelength_values >= min_wavelength) & (
+        wavelength_values <= max_wavelength
     )
-    resampler = FluxConservingResampler()
-    return resampler(input_spectrum, wavelength_range)
+    xs = wavelength_values[within_range]
+    ys = y_values[within_range]
+
+    max_num_points = MAX_NUM_PLOT_POINTS
+    if len(xs) > max_num_points:
+        # If there are too many wavelengths, downsample the points, using linear
+        # interpolation for the resampling.
+        resampled_xs = np.linspace(xs[0], xs[-1], max_num_points)
+        resampled_ys = np.interp(resampled_xs, xs, ys)
+        return resampled_xs.tolist(), resampled_ys.tolist()
+    else:
+        # No resampling is necessary.
+        return xs.tolist(), ys.tolist()
