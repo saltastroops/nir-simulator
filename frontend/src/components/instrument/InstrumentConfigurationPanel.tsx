69330d860b4739764a583a2a3e1b79dac21a1016
Eb-Zeero <macebelene@gmail.com>
2024-04-18 12:00:47 +0200

    rename filter name from clear-filter to Clear Filter

--- frontend/src/components/instrument/InstrumentConfigurationPanel.tsx
+++ frontend/src/components/instrument/InstrumentConfigurationPanel.tsx
@@ -193,7 +193,7 @@ export function InstrumentConfigurationPanel({
                     }
                     name="filter"
                   >
-                    <option value={"clear-filter"}>Clear Filter</option>
+                    <option value={"Clear Filter"}>Clear Filter</option>
                     <option value={"lwbf"}>LWBF</option>
                   </select>
                 </div>
